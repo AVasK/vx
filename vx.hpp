e00bd16d5d1e8b84291f26ea7665605083b69a3e
Alex <avask.work@gmail.com>
2022-06-16 15:05:45 +0300

    support for C++17 added + a few fixes
    - using lang_features macros instead of hardcoded dates
    - renamed `convert_to` -> `as_t`
    - renamed `at_index` -> `at_t`
    - enclosed `requires` in #if to support C++17

--- vx.hpp
+++ vx.hpp
@@ -40,57 +40,57 @@ constexpr std::optional<size_t> try_find(size_t index=0) {
 }
 
 // =====[ at ]=====
-template <size_t I> struct at_index : std::in_place_index_t<I> {};
-template <size_t I> inline constexpr at_index<I> at;
+template <size_t I> struct at_t : std::in_place_index_t<I> {};
+template <size_t I> inline constexpr at_t<I> at;
 
 template <typename T, size_t I>
-#if __cplusplus/100 >= 2020
+#if defined __cpp_concepts && __cplusplus >= __cpp_concepts
 requires requires (T object) { { std::get<I>(object) }; }
 #endif
-decltype(auto) operator| (T && v, at_index<I>) {
+decltype(auto) operator| (T && v, at_t<I>) {
     return std::get<I>(std::forward<T>(v));
 }
 
 
 // =====[ as ]=====
-template <typename T> struct convert_to : std::in_place_type_t<T> {};
-template <typename T> inline constexpr convert_to<T> as;
+template <typename T> struct as_t : std::in_place_type_t<T> {};
+template <typename T> inline constexpr as_t<T> as;
 
 // generic case : as acts as a static_cast (for non-variant types)
 template <typename From, typename To>
-constexpr auto operator| (From const& value, convert_to<To>) {
+constexpr auto operator| (From const& value, as_t<To>) {
     return static_cast<To>(value);
 }
 
 // =====[ variant|as ]===== 
 template <typename... Ts, typename Type>
-constexpr decltype(auto) operator| (std::variant<Ts...> & variant, convert_to<Type>) {
+constexpr decltype(auto) operator| (std::variant<Ts...> & variant, as_t<Type>) {
     return std::get<Type>(variant);
 }
 
 template <typename... Ts, typename Type>
-constexpr decltype(auto) operator| (std::variant<Ts...> const& variant, convert_to<Type>) {
+constexpr decltype(auto) operator| (std::variant<Ts...> const& variant, as_t<Type>) {
     return std::get<Type>(variant);
 }
 
 template <typename... Ts, typename Type>
-constexpr decltype(auto) operator| (std::variant<Ts...> && variant, convert_to<Type>) {
+constexpr decltype(auto) operator| (std::variant<Ts...> && variant, as_t<Type>) {
     return std::get<Type>(std::move(variant));
 }
 
 // =====[ any|as ]===== 
 template <typename Type>
-constexpr decltype(auto) operator| (std::any & a, convert_to<Type>) {
+constexpr decltype(auto) operator| (std::any & a, as_t<Type>) {
     return std::any_cast<Type>(a);
 }
 
 template <typename Type>
-constexpr decltype(auto) operator| (std::any const& a, convert_to<Type>) {
+constexpr decltype(auto) operator| (std::any const& a, as_t<Type>) {
     return std::any_cast<Type>(a);
 }
 
 template <typename Type>
-constexpr decltype(auto) operator| (std::any && a, convert_to<Type>) {
+constexpr decltype(auto) operator| (std::any && a, as_t<Type>) {
     return std::any_cast<Type>(std::move(a));
 }
 
@@ -101,7 +101,7 @@ template <typename T> inline constexpr compare<T> is {};
 
 // =====[ variant|is ]=====
 template <typename... Ts, typename Type>
-#if __cplusplus/100 >= 2020
+#if defined __cpp_concepts && __cplusplus >= __cpp_concepts
 requires( try_find<Type, Ts...>() |as <bool> )
 #endif
 constexpr bool  operator| (std::variant<Ts...> const& variant, compare<Type>) {
@@ -132,7 +132,9 @@ constexpr decltype(auto) operator| (std::variant<Ts...> const& v, match<Fs...> c
 
 // =====[ optional match ]=====
 template <typename T, typename... Fs>
+#if defined __cpp_concepts && __cplusplus >= __cpp_concepts
 requires (std::is_invocable_v<match<Fs...>, T> && std::is_invocable_v<match<Fs...>>)//&& requires(match<Fs...> const& match){ {match()}; })
+#endif
 constexpr decltype(auto) operator| (std::optional<T> const& o, match<Fs...> const& match) {
     if (o.has_value()) return match(o.value());
     else return match();
